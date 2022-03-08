b24bd7ae025a43ca7f3af175eb784c560e7e52f8
CampanelliEmanuele <emanuele.campanelli@studio.unibo.it>
2022-03-08 17:43:04 +0100

    Reformat: revolution ☭

--- sistemi-operativi/s1_c/essentials/pointer/example.c
+++ sistemi-operativi/s1_c/essentials/pointer/example.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int func (int arr[]) {
     return 0;
@@ -14,7 +15,7 @@ int main () {
         printf ("*p = &m  -->  p* = %d\n", *p);
         printf ("n  = *p  -->  n  = %d\n", n);
         // n=&m;                /* This will cause an error! */
-        printf ("&m = %d\n", &m);
+        printf ("&m = %ls\n", &m);
 
     // Pointer and array
         int arr[10], *p1, *p2;
@@ -24,11 +25,28 @@ int main () {
         *p1 = 40;       /* arr[4] = 40 */
         *p2 = *p1;      /* arr[5] = arr[4] */
 
-        printf ("&arr[4] = %d\n", &arr[4]);
+        printf ("&arr[4] = %ls\n", &arr[4]);
+        printf ("\n");
     
     // Pointers and first array element
         func (arr); // This line is equal to: func (&arr[0])
         // arr is a pointer to te first element
+
+    // Others about pointers
+        char *str = malloc(5);
+        str[0] = 'y';   /* value: y  - address 0x23 */
+        str[1] = 'e';   /* value: e  - address 0x24 */
+        str[2] = 'a';   /* value: a  - address 0x25 */
+        str[3] = 'h';   /* value: h  - address 0x26 */
+        str[4] = '\0';  /* value: \0 - address 0x27 */
+        free(str);      /* All done now */
+
+        int tmp = 420;                  /* tmp: value = 420 - address: 0xaddres1 */
+        int *tmpPointer = &val;         /* tmpPointer: value = 0xaddres1 - address: 0xaddres2 */
+        if (&tmp == tmpPointer)
+            printf ("tmpPointer = %ls\n", tmpPointer);
+        printf ("\n");
+
 
     return 0;
 }
